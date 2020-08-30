6fd6924ca4e2f6ebe8015daac79cf8aee0333ec3
Peter Cheng <petercheng7788@gmail.com>
2020-08-30 19:49:27 +0800

    no message

--- src/components/chatroom/addChatRoomPopUp.tsx
+++ src/components/chatroom/addChatRoomPopUp.tsx
@@ -20,7 +20,7 @@ const AddChatRoomPopUp: React.FC<{
     (state: RootState) => state.userInfo
   );
   const createRoom = async () => {
-    console.log(userId);
+    console.log("userId  ", userId);
     const { data } = await axios.post(
       `${process.env.REACT_APP_API_URL}/user/createNewRoom`,
       {
